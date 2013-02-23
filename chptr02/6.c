ed910190e24ac8e751bd9ab244f5582cbca09141
awjrichards <arichards@wikimedia.org>
2013-02-22 17:52:05 -0700

    Project 6

---
+++ chptr02/6.c
@@ -0,0 +1,17 @@
+/**
+ * Modify the program of Programming Project 5 so that the polynomial is
+ * evaluated using the following formula:
+ * ((((3x+2)x-5)x-1)x+7)x-6
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	float x, result;
+	printf( "Calculate the value of 3x^5 + 2x^4 - 5x^3 - x^2 + 7x -6\n" );
+	printf( "Please enter a value for x: " );
+	scanf( "%f", &x );
+	result = ((((3*x+2)*x-5)*x-1)*x+7)*x-6;
+	printf( "Result: %.2f\n", result );
+	return 0;
+}
